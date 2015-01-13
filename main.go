e94845f933a13521f2727ae93a92df459cacacc1
Scott Redig <Laremere@gmail.com>
2015-01-13 12:33:11 -0600

    Add main.go and config file reading
    Main.go contains the main function for the vrpn_webapp tool.
    Config struct for reading in the new format of config.toml
    Types for each type of device, with accessor methods to be used for
    creating the device objects to be used in the future.

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+	//The main config object
+	var config Config
+
+	//Decode the config file
+	_, err := toml.DecodeFile("config/config.toml", &config)
+	if err != nil {
+		//Report error and exit.
+		fmt.Println("Error reading the config file, exiting.")
+		fmt.Println(err)
+		return
+	}
+
+	//Temporary debug
+	fmt.Println(config)
+}
+
+//The root for configuration
+type Config struct {
+	HttpPort uint16
+	VrpnPort uint16
+	Buttons  []Button  `toml:"button"`
+	Toggles  []Toggle  `toml:"toggle"`
+	Sliders  []Slider  `toml:"slider"`
+	Spinners []Spinner `toml:"spinner"`
+}
+
+//Interface for reading device's information
+type DeviceConfig interface {
+	GetName() string    //Device Name
+	GetInitial() string //Initial Value
+}
+
+//Represents a clickable button.
+//See the default config.toml for documentation on components.
+type Button struct {
+	Name    string
+	Display string
+}
+
+func (b *Button) GetName() string {
+	return b.Name
+}
+
+func (b *Button) GetInitial() string {
+	return "false"
+}
+
+//Represents a toggle box.
+//See the default config.toml for documentation on components.
+type Toggle struct {
+	Name    string
+	Display string
+	Initial bool
+}
+
+func (t *Toggle) GetName() string {
+	return t.Name
+}
+
+func (t *Toggle) GetInitial() string {
+	return fmt.Sprint(t.Initial)
+}
+
+//Represents a slider bar.
+//See the default config.toml for documentation on components.
+type Slider struct {
+	Name    string
+	Display string
+	Range   [2]float64
+	Initial float64
+	Step    float64
+}
+
+func (s *Slider) GetName() string {
+	return s.Name
+}
+
+func (s *Slider) GetInitial() string {
+	return fmt.Sprint(s.Initial)
+}
+
+//Represents a number selector.
+//See the default config.toml for documentation on components.
+type Spinner struct {
+	Name    string
+	Display string
+	Range   [2]float64
+	Initial float64
+	Step    float64
+}
+
+func (s *Spinner) GetName() string {
+	return s.Name
+}
+
+func (s *Spinner) GetInitial() string {
+	return fmt.Sprint(s.Initial)
+}
